e6ee729055f058c6479772a146e1dc6ad667f612
Olukay-05 <olukayode.ogunnowo@yahoo.com>
2022-12-26 22:26:04 +0100

    Cart debugged

--- Kay's-furnitures/app.js
+++ Kay's-furnitures/app.js
@@ -112,6 +112,7 @@ class UI {
                 this.addCartItem(cartItem);
 
                 //show the cart
+                this.showCart();
             });
         });
     }
@@ -132,6 +133,57 @@ class UI {
 
     addCartItem(item) {
         const div = document.createElement('div');
+        div.classList.add("cart-item");
+        div.innerHTML = `
+        <img src="${item.image}" alt="product">
+        <div>
+            <h4>${item.title}</h4>
+            <h5>$${item.price}</h5>
+            <span class="remove-item" data-id=${item.id}>remove</span>
+         </div>
+         <div>
+            <i class="fas fa-chevron-up" data-id=${item.id}></i>
+            <p class="item-amount">${item.amount}</p>
+            <i class="fas fa-chevron-down" data-id=${item.id}></i>
+         </div>
+        `;
+        cartContent.appendChild(div);
+
+    }
+
+    showCart() {
+        cartOverlay.classList.add("transparentBcg");
+        cartDOM.classList.add("showCart");
+
+    }
+
+    setupAPP() {
+
+        cart = Storage.getCart();
+        this.setCartValues(cart);
+        this.populateCart(cart);
+        cartBtn.addEventListener('click', this.showCart);
+        closeCartBtn.addEventListener('click', this.hideCart)
+    }
+
+    populateCart() {
+        cart.forEach(item => this.addCartItem(item));
+    }
+
+    hideCart() {
+
+        cartOverlay.classList.remove("transparentBcg");
+        cartDOM.classList.remove("showCart");
+
+    }
+
+    cartLogic() {
+
+        clearCartBtn.addEventListener('click', () => {this.clearCart();});
+    }
+
+    clearCart() {
+
     }
 
 }
@@ -154,6 +206,10 @@ class Storage {
 
         localStorage.setItem("cart", JSON.stringify(cart))
     }
+
+    static getCart() {
+        return localStorage.getItem('cart') ? JSON.parse(localStorage.getItem("cart")) : []
+    }
 }
 
 
@@ -161,6 +217,9 @@ class Storage {
 document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products();
+
+    //setup app
+    ui.setupAPP();
 
     // get all products
     products.getProducts().then(products => {
